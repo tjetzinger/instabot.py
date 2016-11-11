33c53cdf5da6a9abcb92b44a346f649e0d8a358f
Thomas Jetzinger <thomas@jetzinger.com>
2016-11-11 21:27:20 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='instabot',
+    version='0.1',
+    packages=[''],
+    package_dir={'': 'src'},
+    url='https://github.com/tjetzinger/instabot.py',
+    license='',
+    author='',
+    author_email='',
+    description='',
+    zip_safe = False,
+    install_requires = [
+        "requests==2.11.1",
+    ]
+)
